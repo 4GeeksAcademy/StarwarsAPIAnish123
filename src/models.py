ed0e599f31c930675f697dd73ac48c00ba962b61
Anish-tech123 <rvermaji321@alumni.iastate.edu>
2024-10-10 22:53:41 +0000

    added new features

--- src/models.py
+++ src/models.py
@@ -2,18 +2,89 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship("Favorite")
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "username": self.username,
+
             # do not serialize the password, its a security breach
         }
+
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    climate = db.Column(db.String(250))
+    terrain = db.Column(db.String(250))
+    population = db.Column(db.Integer)
+    favorites = db.relationship("Favorite")
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "terrain": self.terrain,
+            "population": self.population,
+            "favorites": self.favorites,
+            # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    height = db.Column(db.Integer)
+    hair_color = db.Column(db.String(250))
+    eye_color = db.Column(db.String(250))
+    gender = db.Column(db.String(250))
+    favorites = db.relationship("Favorite")
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "gender": self.gender,
+            "favorites": self.favorites,
+            # do not serialize the password, its a security breach
+        }
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    character_id = db.Column(db.Integer, db.ForeignKey('character.id'))
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planet_id": self.planet_id,
+            "character_id": self.character_id,
+            # do not serialize the password, its a security breach
+        }
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/routes.py
@@ -0,0 +1,133 @@
+"""
+This module takes care of starting the API Server, Loading the DB and Adding the endpoints
+"""
+from flask import Flask, request, jsonify, url_for, Blueprint
+from models import db, User,Planet, Character, Favorite
+from utils import generate_sitemap, APIException
+from flask_cors import CORS
+
+api = Blueprint('api', __name__)
+
+# Allow CORS requests to this API
+CORS(api)
+
+
+
+@api.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([user.serialize() for user in users]), 200
+
+@api.route('/users/', methods=['POST'])
+def create_user():
+    request_body = request.get_json()
+    user = User(username=request_body["username"], password=request_body["password"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/<int:id>', methods=['GET'])
+def get_single_user(id):
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
+
+@api.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(user.serialize(),'deleted'), 200
+
+
+
+
+@api.route('/planets/<int:id>', methods=['GET'])
+def get_single_planet(id):
+    planet = Planet.query.get(id)
+    return jsonify(planet.serialize()), 200
+
+@api.route('/planets', methods=['GET'])
+def get_planets():
+    planets = Planet.query.all()
+    return jsonify([planet.serialize() for planet in planets]), 200
+
+@api.route('/planets', methods=['POST'])
+def create_planet():
+    request_body = request.get_json()
+    planet = Planet(name=request_body["name"], climate=request_body["climate"], terrain=request_body["terrain"], population=request_body["population"])
+    db.session.add(planet)
+    db.session.commit()
+    return jsonify(planet.serialize()), 200
+
+@api.route('/planets/<int:id>', methods=['DELETE'])
+def delete_planet(id):
+    planet = Planet.query.get(id)
+    db.session.delete(planet)
+    db.session.commit()
+    return jsonify(planet.serialize(),'deleted'), 200
+
+# Character Routes
+@api.route('/characters', methods=['GET'])
+def get_characters():
+    characters = Character.query.all()
+    return jsonify([character.serialize() for character in characters]), 200
+
+@api.route('/characters', methods=['POST'])
+def create_character():
+    request_body = request.get_json()
+    character =  Character(name=request_body["name"], height=request_body['height'], hair_color=request_body['hair_color'], eye_color=request_body['eye_color'], gender=request_body['gender'])
+    db.session.add(character)
+    db.session.commit()
+    return jsonify(character.serialize()), 200
+
+@api.route('/character/<int:character_id>', methods=['GET'])
+def get_single_character(character_id):
+    character = Character.query.get(character_id)
+    return jsonify(character.serialize()), 200
+
+@api.route('/character/<int:character_id>', methods=['DELETE'])
+def delete_character(character_id):
+    character = Character.query.get(character_id)
+    db.session.delete(character)
+    db.session.commit()
+    return jsonify(character.serialize(),'deleted'), 200
+
+# Favorite Routes
+@api.route('/favorites', methods=['GET'])
+def get_favorites():
+    favorites = Favorite.query.all()
+    return jsonify([favorite.serialize() for favorite in favorites]), 200
+
+@api.route('/favorite', methods=['POST'])
+def create_favorite():
+    request_body = request.get_json()
+    favorite = Favorite(user_id=request_body["user_id"], planet_id=request_body["planet_id"], character_id=request_body["character_id"])
+    db.session.add(favorite)
+    db.session.commit()
+    return jsonify(favorite.serialize()), 200
+
+@api.route('/favorite/<int:favorite_id>', methods=['GET'])
+def get_single_favorite(favorite_id):
+    favorite = Favorite.query.get(favorite_id)
+    return jsonify(favorite.serialize()), 200
+
+@api.route('/favorite/<int:favorite_id>', methods=['DELETE'])
+def delete_favorite(favorite_id):
+    favorite = Favorite.query.get(favorite_id)
+    db.session.delete(favorite)
+    db.session.commit()
+    return jsonify(favorite.serialize(),'deleted'), 200
+
+
+
+
+
+
+
+
+
+
+
+
+
+
